10be44de9b9725efa260a2ca63e9243a1f7d7ac8
shinsukeoouchi <otoutyann0103@gmail.com>
2025-07-16 20:37:57 +0900

    Update Book.java
    アクセサを追加

--- Book.java
+++ Book.java
@@ -1,8 +1,16 @@
 public class Book extends TangibleAsset{
 	private String isbn;
+	private String name;
+	private int price;
+	private String color;
+
 	public Book(String name,int price,String color,String isbn){
 		super(name,price,color);
 		this.isbn = isbn;
 	}
 	public String getIsbn(){return this.isbn;}
+	public String getName(){return this.name;}
+	public int getPrice(){return this.price;}
+	public int getColor(){return this.color;}
+	
 }
