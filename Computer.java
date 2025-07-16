31d66d65697d3890c9aa5d8fa5e8d30861c7c099
shinsukeoouchi <otoutyann0103@gmail.com>
2025-07-16 19:46:28 +0900

    Computer.java

--- Computer.java
+++ Computer.java
@@ -1,24 +1,20 @@
 public class Computer extends TangibleAsset {
 	private String name;
 	private int price;
-  private String color;
+	private String color;
   
-	//コンストラクタ
-	public Computer(String name, int price, String color, String name) {
+	public Computer(String name, int price, String color) {
 		super(name, price, color);
 		this.name = name;
 	}
-  @Override
 	public String getName() {
 		return this.name;
 	}
   
-	@Override
 	public void getPrice(int price) {
 		this.price = price;
 	}
   
-	@Override
 	public String getColor() {
 		return this.color;
 	}
